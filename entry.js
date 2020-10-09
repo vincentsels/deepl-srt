6815f239d7e2e4496dbcf59041a4a7cb1b563ebb
Vincent Sels <vincent_sels@hotmail.com>
2020-10-09 23:35:15 +0200

    Works

--- entry.js
+++ entry.js
@@ -1,10 +1,21 @@
 class Entry {
   constructor(text) {
+    if (!text) return;
     const entryLines = text.split(/\r?\n/)
     this.lineNumber = entryLines[0];
     this.ts = entryLines[1];
     this.lines = entryLines.slice(2);
     this.total = this.lines.join(' ');
+  }
+
+  toText() {
+    let text = this.lineNumber + '\n' +
+      this.ts + '\n';
+    for (let line of this.lines) {
+      text += line + '\n';
+    }
+    text += '\n';
+    return text;
   }
 }
 
--- index.js
+++ index.js
@@ -38,15 +38,17 @@ log('Target language:', target);
 log('Formality:', formality);
 
 const allInputAsText = fs.readFileSync(input).toString();
+if (!allInputAsText || allInputAsText.length === 0 || isNaN(allInputAsText.charAt(0))) throw new Error('Does not look like an srt file');
+
 const allEntriesAsText = allInputAsText.split(/\r?\n\r?\n/).filter(x => x);
 const allEntries = allEntriesAsText.map(e => new Entry(e));
 
 const amtOfLines = Math.max(...allEntries.map(e => e.lines.length));
 const maxLineLength = Math.max(...allEntries.flatMap(e => e.lines.map(l => l.length)));
 
-log('Entries: ', allEntries.length);
-log('Lines per entry: ', amtOfLines);
-log('Max length of line: ', maxLineLength);
+log('Entries:', allEntries.length);
+log('Lines per entry:', amtOfLines);
+log('Max length of line:', maxLineLength);
 
 let currentSentence = '';
 let currentTimestamps = [];
@@ -84,7 +86,7 @@ for (let currentEntry of allEntries) {
 
 // In case the thing doesn't end in a sentence, push the rest
 if (currentSentence) {
-  results.push({ currentSentence, ts: [...currentTimestamps] });
+  results.push({ sentence: currentSentence, ts: [...currentTimestamps] });
 }
 
 log(results);
@@ -110,12 +112,63 @@ axios.default.post('https://api.deepl.com/v2/translate', params.toString())
       const translations = response.data.translations.map(t => t.text);
       log(translations);
 
-      
+      const targetEntries = allEntries.map((e) => {
+        const trans = new Entry();
+        trans.lineNumber = e.lineNumber;
+        trans.lines = [];
+        trans.ts = e.ts;
+        return trans;
+      });
+
+      let overflow = false;
+
+      translations.forEach((trans, i) => {
+        const ts = results[i].ts[0];
+        const words = trans.split(' ');
+
+        let targetEntry = targetEntries.find(e => e.ts >= ts);
+
+        for (let word of words) {
+          if (targetEntry.lines.length === amtOfLines
+            && targetEntry.lines[amtOfLines - 1].length + word.length + 1 > maxLineLength) {
+            // Entry full, go to the next one
+            log('Entry', targetEntry.lineNumber, 'full, go to next one'); // TODO remove
+            targetEntry = targetEntries.find(e => e.ts > targetEntry.ts);
+            if (targetEntry === undefined) {
+              // We're at the last one - just keep appending to it...
+              log('Entries full, appending to last one...'); // TODO remove
+              overflow = true;
+              targetEntry = targetEntries.find(e => e.ts >= ts);
+            } else {
+              log('Entry', targetEntry.lineNumber, 'selected'); // TODO remove
+            }
+          }
+
+          if (targetEntry.lines.length === 0) {
+            // First word in this entry
+            log('Adding first word', word, 'to', targetEntry.lineNumber); // TODO remove
+            targetEntry.lines.push(word);
+          } else if ((overflow || targetEntry.lines.length < amtOfLines)
+            && targetEntry.lines[targetEntry.lines.length - 1].length + word.length + 1 > maxLineLength) {
+            // Line in entry full, but place for a new one... Add new line in this entry
+            log('Adding first word', word, 'to new line of', targetEntry.lineNumber); // TODO remove
+            targetEntry.lines.push(word);
+          } else {
+            // No problem to add this word to the last line in this entry
+            log('Adding word', word, 'to', targetEntry.lineNumber); // TODO remove
+            targetEntry.lines[targetEntry.lines.length - 1] += " " + word;
+          }
+        }
+      });
+
+      log('Done, writing file...');
+      fs.writeFileSync(output, targetEntries.reduce((p, c) => p + c.toText(), ''));
+      log('Finished.');
     }
   })
   .catch((err) => { 
-    console.error('Axios request failed', err);
-    throw new Error(err)
+    console.error('Translation failed', err);
+    throw new Error(err);
   });
 
 function getSrtFileInFolder() {
