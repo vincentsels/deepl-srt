ae2bebab8e93cf701cf922a03df0e35215ed4fa6
Vincent Sels <vincent_sels@hotmail.com>
2020-10-09 22:21:05 +0200

    Translate

--- index.js
+++ index.js
@@ -3,29 +3,39 @@ const commandLineArgs = require('command-line-args');
 const path = require('path')
 const fs = require('fs');
 const Entry = require('./entry');
+const axios = require('axios');
+const { env } = require('process');
 
 const optionDefinitions = [
-    { name: 'input', alias: 'i', type: String },
-    { name: 'output', alias: 'o', type: String },
-    { name: 'from', alias: 'f', type: String },
-    { name: 'to', alias: 't', type: String },
-  ]
+  { name: 'input', alias: 'i', type: String },
+  { name: 'output', alias: 'o', type: String },
+  { name: 'source', alias: 's', type: String },
+  { name: 'target', alias: 't', type: String },
+  { name: 'key', alias: 'k', type: String },
+  { name: 'formal', alias: 'f', type: Number },
+  { name: 'verbose', alias: 'v', type: Number },
+];
 
 const options = commandLineArgs(optionDefinitions);
 
+const key = options.key || env.DEEPL_API_KEY;
 const input = options.input || getSrtFileInFolder();
 let output = options.output;
-const from = options.from || 'NL';
-const to = options.to || 'FR';
+const source = options.from || 'NL';
+const target = options.to || 'FR';
+const formality = options.formal ? 'more' : 'less';
+const verbose = true; // TODO
 
-if (!input) throw new Error('At least specify input file with --input or -i');
+if (!key) throw new Error('Specify a DeepL API key as DEEPL_API_KEY environment variable, or using the --key or -k parameter.')
+if (!input) throw new Error('At least specify input file with --input or -i.');
 
-if (!output) output = input.split('.').slice(0, -1).join('.') + '.' + to.toLowerCase() + '.srt';
+if (!output) output = input.split('.').slice(0, -1).join('.') + '.' + target.toLowerCase() + '.srt';
 
-console.log('Input file:', input);
-console.log('Output file:', output);
-console.log('From language:', from);
-console.log('To language:', to);
+log('Input file:', input);
+log('Output file:', output);
+log('Source language:', source);
+log('Target language:', target);
+log('Formality:', formality);
 
 const allInputAsText = fs.readFileSync(input).toString();
 const allEntriesAsText = allInputAsText.split(/\r?\n\r?\n/).filter(x => x);
@@ -34,9 +44,9 @@ const allEntries = allEntriesAsText.map(e => new Entry(e));
 const amtOfLines = Math.max(...allEntries.map(e => e.lines.length));
 const maxLineLength = Math.max(...allEntries.flatMap(e => e.lines.map(l => l.length)));
 
-console.log('Entries: ', allEntries.length);
-console.log('Lines per entry: ', amtOfLines);
-console.log('Max length of line: ', maxLineLength);
+log('Entries: ', allEntries.length);
+log('Lines per entry: ', amtOfLines);
+log('Max length of line: ', maxLineLength);
 
 let currentSentence = '';
 let currentTimestamps = [];
@@ -77,7 +87,36 @@ if (currentSentence) {
   results.push({ currentSentence, ts: [...currentTimestamps] });
 }
 
-console.log(results);
+log(results);
+
+const params = new URLSearchParams();
+params.append('auth_key', key);
+params.append('target_lang', target);
+params.append('source_lang', source);
+params.append('split_sentences', 0);
+params.append('formality', formality);
+const sentencesToTranslate = results.forEach(r => {
+  params.append('text', r.sentence);
+});
+
+log('Translating...');
+
+axios.default.post('https://api.deepl.com/v2/translate', params.toString())
+  .then((response) => {
+    if (response.status !== 200) {
+      console.error('Request to DeepL failed', response);
+      throw new Error(response.data);
+    } else {
+      const translations = response.data.translations.map(t => t.text);
+      log(translations);
+
+      
+    }
+  })
+  .catch((err) => { 
+    console.error('Axios request failed', err);
+    throw new Error(err)
+  });
 
 function getSrtFileInFolder() {
   const files = fs.readdirSync('.');
@@ -89,4 +128,8 @@ function getSrtFileInFolder() {
     };
   }
   return null;
+}
+
+function log(...args) {
+  if (verbose) console.log(...args);
 }
