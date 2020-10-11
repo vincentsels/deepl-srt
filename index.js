82e17bc64a949ec00196f52514f234d76b720c69
Sels Vincent <vincent.sels@infrabel.be>
2020-10-11 13:53:46 +0200

    Some fixes

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const optionDefinitions = [
   { name: 'source', alias: 's', type: String },
   { name: 'target', alias: 't', type: String },
   { name: 'key', alias: 'k', type: String },
-  { name: 'formal', alias: 'f', type: Number },
-  { name: 'verbose', alias: 'v', type: Number },
+  { name: 'formal', alias: 'f', type: Boolean },
+  { name: 'debug', alias: 'd', type: Boolean },
+  { name: 'lines', alias: 'l', type: Number },
+  { name: 'maxlength', alias: 'm', type: Number },
 ];
 
 const options = commandLineArgs(optionDefinitions);
@@ -24,7 +26,7 @@ let output = options.output;
 const source = options.from || 'NL';
 const target = options.to || 'FR';
 const formality = options.formal ? 'more' : 'less';
-const verbose = true; // TODO
+const logDebug = options.debug;
 
 if (!key) throw new Error('Specify a DeepL API key as DEEPL_API_KEY environment variable, or using the --key or -k parameter.')
 if (!input) throw new Error('At least specify input file with --input or -i.');
@@ -38,13 +40,14 @@ log('Target language:', target);
 log('Formality:', formality);
 
 const allInputAsText = fs.readFileSync(input).toString();
+
 if (!allInputAsText || allInputAsText.length === 0 || isNaN(allInputAsText.charAt(0))) throw new Error('Does not look like an srt file');
 
-const allEntriesAsText = allInputAsText.split(/\r?\n\r?\n/).filter(x => x);
+const allEntriesAsText = allInputAsText.split(/\r?\n\r?\n/).filter(x => x.trim());
 const allEntries = allEntriesAsText.map(e => new Entry(e));
 
-const amtOfLines = Math.max(...allEntries.map(e => e.lines.length));
-const maxLineLength = Math.max(...allEntries.flatMap(e => e.lines.map(l => l.length)));
+const amtOfLines = options.lines || Math.max(...allEntries.map(e => e.lines.length));
+const maxLineLength = options.maxlength || Math.max(...allEntries.flatMap(e => e.lines.map(l => l.length)));
 
 log('Entries:', allEntries.length);
 log('Lines per entry:', amtOfLines);
@@ -81,7 +84,7 @@ for (let currentEntry of allEntries) {
     rest = currentEntry.total;
   }
 
-  currentSentence = currentSentence + rest;
+  currentSentence = currentSentence ? (currentSentence + ' ' + rest) : rest;
 }
 
 // In case the thing doesn't end in a sentence, push the rest
@@ -89,7 +92,7 @@ if (currentSentence) {
   results.push({ sentence: currentSentence, ts: [...currentTimestamps] });
 }
 
-log(results);
+debug(results);
 
 const params = new URLSearchParams();
 params.append('auth_key', key);
@@ -110,7 +113,7 @@ axios.default.post('https://api.deepl.com/v2/translate', params.toString())
       throw new Error(response.data);
     } else {
       const translations = response.data.translations.map(t => t.text);
-      log(translations);
+      debug(translations);
 
       const targetEntries = allEntries.map((e) => {
         const trans = new Entry();
@@ -132,30 +135,30 @@ axios.default.post('https://api.deepl.com/v2/translate', params.toString())
           if (targetEntry.lines.length === amtOfLines
             && targetEntry.lines[amtOfLines - 1].length + word.length + 1 > maxLineLength) {
             // Entry full, go to the next one
-            log('Entry', targetEntry.lineNumber, 'full, go to next one'); // TODO remove
+            debug('Entry', targetEntry.lineNumber, 'full, go to next one'); // TODO remove
             targetEntry = targetEntries.find(e => e.ts > targetEntry.ts);
             if (targetEntry === undefined) {
               // We're at the last one - just keep appending to it...
-              log('Entries full, appending to last one...'); // TODO remove
+              debug('Entries full, appending to last one...'); // TODO remove
               overflow = true;
               targetEntry = targetEntries.find(e => e.ts >= ts);
             } else {
-              log('Entry', targetEntry.lineNumber, 'selected'); // TODO remove
+              debug('Entry', targetEntry.lineNumber, 'selected'); // TODO remove
             }
           }
 
           if (targetEntry.lines.length === 0) {
             // First word in this entry
-            log('Adding first word', word, 'to', targetEntry.lineNumber); // TODO remove
+            debug('Adding first word', word, 'to', targetEntry.lineNumber); // TODO remove
             targetEntry.lines.push(word);
           } else if ((overflow || targetEntry.lines.length < amtOfLines)
             && targetEntry.lines[targetEntry.lines.length - 1].length + word.length + 1 > maxLineLength) {
             // Line in entry full, but place for a new one... Add new line in this entry
-            log('Adding first word', word, 'to new line of', targetEntry.lineNumber); // TODO remove
+            debug('Adding first word', word, 'to new line of', targetEntry.lineNumber); // TODO remove
             targetEntry.lines.push(word);
           } else {
             // No problem to add this word to the last line in this entry
-            log('Adding word', word, 'to', targetEntry.lineNumber); // TODO remove
+            debug('Adding word', word, 'to', targetEntry.lineNumber); // TODO remove
             targetEntry.lines[targetEntry.lines.length - 1] += " " + word;
           }
         }
@@ -184,5 +187,9 @@ function getSrtFileInFolder() {
 }
 
 function log(...args) {
-  if (verbose) console.log(...args);
+  console.log(...args);
+}
+
+function debug(...args) {
+  if (logDebug) console.log(...args);
 }
